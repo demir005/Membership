05f7097a3447429360a0efe20e3a3075bdd6fbc3
demir005 <demirmah93@gmail.com>
2018-10-20 12:27:28 +0200

    ASP.NET MVC 5 EnityFramework
    ASP.NET MVC 5 EnityFramework

---
+++ Scripts/forgot-password.js
@@ -0,0 +1,24 @@
+﻿$(function () {
+    var pwdLinkHover = $('#pwdLink').hover(onCloseLogin);
+    var resetPwd = $('#resetPwd').click(onResetPassword);
+
+    function onCloseLogin() {
+        $('div[data-login-user-panel-area]').removeClass('open');
+    }
+
+    function onResetPassword() {
+        var email = $('.modal-dialog .reset-email').val();
+        var antiforgery = $('[name="__RequestVerificationToken"]').val();
+        var url = "/Account/ForgotPasswordConfirmation";
+
+        // alert(email + " " + antiforgery + " " + url)
+
+        $.post("/Account/ForgotPassword",  {
+            __RequestVerificationToken: antiforgery, email: email
+        }, function (data) {
+            location.href = url;
+        }).fail(function (xhr, status, error) {
+            location.href = url;
+        });
+    }
+});
